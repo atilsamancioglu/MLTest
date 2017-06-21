d265b2711bd0a86ab37f2be07038dd434d7e8d37
Atil Samancioglu <samancioglu.atil@gmail.com>
2017-06-21 16:57:51 +0300

    last

--- Tst/ViewController.swift
+++ Tst/ViewController.swift
@@ -7,19 +7,76 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func chooseButtonClicked(_ sender: Any) {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true, completion: nil)
+        
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imageView.image = info[UIImagePickerControllerEditedImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+        
+        guard let ciImage = CIImage(image: imageView.image!) else {
+            fatalError("couldn't convert UIImage to CIImage")
+        }
+        
+        detectScene(image: ciImage)
+        
+    }
+    
+    func detectScene(image: CIImage) {
+        answerLabel.text = "detecting scene..."
+        
+        // Load the ML model through its generated class
+        if let model = try? VNCoreMLModel(for: GoogLeNetPlaces().model) {
+            
+            let reques = VNCoreMLRequest(model: model, completionHandler: { (request, error) in
+                
+                if let results = request.results as? [VNClassificationObservation] {
+                    let topResult = results.first
+                    
+                    DispatchQueue.main.async {
+                        
+                        let conf = (topResult?.confidence)! * 100
+                        
+                        let rounded = Int (conf * 100 ) / 100
+                        
+                        self.answerLabel.text = "\(rounded)% it's \(String(describing: topResult!.identifier))"
+                    }
+                }
+            })
+            
+            let handler = VNImageRequestHandler(ciImage: image)
+            DispatchQueue.global(qos: .userInteractive).async {
+                do {
+                    try handler.perform([reques])
+                } catch {
+                    print(error)
+                }
+            }
+            
+            
+        }
+    }
+    
 }
 
